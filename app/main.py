b7f846daf56557ba2836021d9a405e6dad6df08f
danielcinome <danielchinome987@gmail.com>
2024-01-24 13:46:26 -0500

    feat: the routers of the modules were included

--- app/main.py
+++ app/main.py
@@ -5,6 +5,12 @@ from fastapi import FastAPI, Request, status
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.api.user import routers as user_routers
+from app.api.login import routers as login_routers
+from app.api.avm import routers as amv_routers
+from app.api.property import routers as property_routers
+from app.api.city import routers as city_routers
+
 
 app = FastAPI(title='Property Management Project')
 
@@ -30,4 +36,29 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+)
+
+app.include_router(
+    login_routers.router,
+    prefix='/login',
+    tags=['Login'],
+)
+app.include_router(
+    user_routers.router,
+    prefix='/user',
+    tags=['User']
+)
+app.include_router(
+    property_routers.router,
+    prefix='/property',
+    tags=['Property']
+)
+app.include_router(
+    city_routers.router,
+    prefix='/city',
+    tags=['City'],
+)
+app.include_router(
+    amv_routers.router,
+    tags=['AVM'],
 )
